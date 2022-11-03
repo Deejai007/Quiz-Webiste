557cb93ce54b509e17a829e3c422b272fd76fb23
Deepak Jain <deepakjain2k19@gmail.com>
2022-11-04 03:41:08 +0530

    Broad Page Design

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    question: "Who is the god of cricket?",
+    answers: [
+      { option: "Virat Kohli", correct: false },
+      { option: "Rohit Sharma", correct: false },
+      { option: "Sachin Tendulkar", correct: true },
+      { option: "Kapil Dev", correct: false },
+    ],
+  },
+];
+function startQuiz() {}
